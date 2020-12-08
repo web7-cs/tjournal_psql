d2874624bbf5ebb36a5cb98615b29487d1699b3f
panuvit.chantara <panuvit.chantara@ait.asia>
2020-12-08 09:27:01 +0700

    v0.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,40 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
-# Create a main sample user.
-# User.create!(username:  "Example User",
-#     email: "test@test.com",
-#     password:              "foobar",
-#     password_confirmation: "foobar",
-#     occupation: "teacher",
-#     age: 30,
-#     school: "SamsenWittayalai school, Bangkok")
+Create a main sample user.
+User.create!(username:  "Example User",
+    email: "test@test.com",
+    password:              "foobar",
+    password_confirmation: "foobar",
+    occupation: "teacher",
+    age: 30,
+    school: "SamsenWittayalai school, Bangkok")
 
-# Generate a bunch of additional users.
-# 99.times do |n|
-# name  = Faker::Name.name
-# email = Faker::Internet.email
-# job = Faker::Job.title
-# school = Faker::Nation.nationality + 'school'
-# password = "password"
-# age = Faker::Number.between(from: 24, to: 55)
-# User.create!(username:  name,
-#     email: email,
-#     password:              "foobar",
-#     password_confirmation: "foobar",
-#     occupation: job,
-#     age: age,
-#     school: school)
-# end
+Generate a bunch of additional users.
+99.times do |n|
+name  = Faker::Name.name
+email = Faker::Internet.email
+job = Faker::Job.title
+school = Faker::Nation.nationality + 'school'
+password = "password"
+age = Faker::Number.between(from: 24, to: 55)
+User.create!(username:  name,
+    email: email,
+    password:              "foobar",
+    password_confirmation: "foobar",
+    occupation: job,
+    age: age,
+    school: school)
+end
 
-# # Generate teaching_ideas for a subset of users.
-# users = User.order(:created_at).take(6)
-# 50.times do
-# content = Faker::Lorem.sentence(word_count: 5)
-# users.each { |user| user.teaching_ideas.create!(content: content) }
-# end
+# Generate teaching_ideas for a subset of users.
+users = User.order(:created_at).take(6)
+50.times do
+content = Faker::Lorem.sentence(word_count: 5)
+users.each { |user| user.teaching_ideas.create!(content: content) }
+end
 
 # Create following relationships
 users = User.all
